3165996a81917418236cd1b2fb2376593225d3e1
lucas <ventura.lucas19@gmail.com>
2018-01-31 18:47:56 -0200

    Scripts for new ubuntu VM config

---
+++ ubuntuConfig.sh
@@ -0,0 +1,9 @@
+#install xubuntu desktop
+sudo apt install xubuntu-desktop
+#get visual code path
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+#install vscode
+sudo apt-get update
+sudo apt-get install code
