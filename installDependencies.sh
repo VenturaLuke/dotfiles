ffdf67963bd7272eb1e9bd91c75901fe31eeabbf
VenturaLuke <ventura.lucas19@gmail.com>
2018-01-31 18:51:07 -0200

    Initial dependencies for react frontend app + mongodb

---
+++ installDependencies.sh
@@ -0,0 +1,22 @@
+#install curl just in case
+sudo apt install curl
+
+#install git
+sudo apt install git
+
+#install nodeJs 8.x version
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install -y build-essential
+
+#install and run mongo server
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+#install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
