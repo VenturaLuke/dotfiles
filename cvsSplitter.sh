aa8a156d94e8e432f718e6b8f1c363470a0d6ea9
VenturaLuke <ventura.lucas19@gmail.com>
2018-04-17 20:50:25 -0300

    Update cvsSplitter.sh
    improving splitter and creating files in new folder

--- cvsSplitter.sh
+++ cvsSplitter.sh
@@ -1,9 +1,13 @@
+echo "File name: "
 read fileName
+echo "Number of lines: "
 read lines
-tail -n +2 $fileName | split -l $lines - diarias_
-for file in diarias_*
+mkdir $fileName
+tail -n +2 ${fileName}.csv | split -l $lines - ${fileName}_
+for file in ${fileName}_*
 do
-    head -n 1 $fileName > tmp_file
+    head -n 1 ${fileName}.csv > tmp_file
     cat $file >> tmp_file
     mv -f tmp_file $file
+    mv $file ${fileName}/${file}.csv
 done
